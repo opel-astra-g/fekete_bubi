87c9791e393894d2ebed51ef594aae9d5a63f387
opel-astra-g <kis.csaba.d@gmail.com>
2025-05-21 12:13:30 +0200

    alap kartya kiosztas

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+import random
+
 jatekmod = int(input("1 vagy 2 játékos: "))
 
 nev1 = ""
@@ -9,3 +11,6 @@ if jatekmod == 1:
 elif jatekmod == 2:
     nev1 = input("Első Játékos neve: ")
     nev2 = input("Második Játékos neve: ")
+
+nev1_kartyak = [random.randint(2, 11) for _ in range(2)]
+nev2_kartyak = [random.randint(2, 11) for _ in range(2)]
